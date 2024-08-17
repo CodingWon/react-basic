a6831935c7c072689e24c27caa7800fe624f2f2a
kjw <kingcoding2298@gmail.com>
2024-08-17 21:01:53 +0900

    React 컴포넌트-1
    - root 컴포넌트 설정
    - 부모 컴포넌트 , 자식 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import "./App.css";
 
+/*
+  컴포넌트 규칙
+    - 첫글자 대문자
+*/
+
+// function Header() {
+//   return (
+//     <header>
+//       <h1>header</h1>
+//     </header>
+//   );
+// }
+
+const Header = () => {
+  // 자식 컴포넌트 .. Footer
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
 function App() {
+  // 부모 컴포넌트 (최상의 조상 - Root 컴포넌트)
   return (
     <>
+      <Header />
       <h1>안녕 리액트</h1>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// const Hello = () => {
+//   return <div>Hello</div>;
+// };
+
+/*
+  관례상 App 컴포넌트를 root 컴포넌트로 설정한다.
+*/
+
 createRoot(document.getElementById("root")).render(<App />);
