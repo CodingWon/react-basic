562185133e5952cca3416b0d66328eac9cbe7158
kjw <kingcoding2298@gmail.com>
2024-08-17 21:29:45 +0900

    JSX로 UI 표현하기 - 2 : 조건에 따라 UI 표현하기

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,29 +10,18 @@
             <img></img> 이미지도 닫힘 태그가 있어야한다.
         4. 최상의 태그는 반드시 하나여야 한다.
 */
-
 const Main = () => {
-  const number = 10;
-  const obj = { a: 1 };
+  const user = {
+    name: "이정한",
+    isLogin: false,
+  };
+  if (user.isLogin) {
+    return <div>로그 아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
 
-  return (
-    // <div></div>
-    <>
-      <img></img>
-      <h1>main</h1>
-      <h2>{number + 10}</h2>
-      <h3>{number % 2 === 0 ? "짝수" : "홀수"}</h3>
-      {10}
-      <br />
-      {/* {if(){}} */}
-      {/* {for(){}} */}
-      {[1, 2, 3]}
-      {true}
-      {undefined}
-      {null}
-      {obj.a}
-    </>
-  );
+  //   return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 };
 
 export default Main;
