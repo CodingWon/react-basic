6b3cbff1348501834eb2921f0f16e62a6a65a826
kjw <kingcoding2298@gmail.com>
2024-08-18 12:13:15 +0900

    5.10) useRef로 컴포넌트의 변수 생성하기 : 수정 횟수 확인하기
    - 리렌더링 할 필요 없는 데이터 다루기

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,4 @@
 import { useState, useRef } from "react";
-// 간단한 회원가입 폼
-/*
-    1. 이름
-    2. 생년월일
-    3. 국적
-    4. 자기소개
-*/
 
 const Register = () => {
   const [input, setInput] = useState({
@@ -15,43 +8,19 @@ const Register = () => {
     bio: "",
   });
 
-  const refObj = useRef(0);
-  console.log(refObj); // {current : 0}
-  console.log(refObj.current); // {current : 0}
-  /*
-    useRef
-    - current라는 property 에 현재 보관할 값을 담아두기만 하는 단순한 js 객체
-    - 값이 변경 되어도 컴포넌트를 리렌더링 시키지 않는다.
- */
+  const countRef = useRef(0);
 
   const onChange = (e) => {
-    console.log("key:", e.target.name, " value:", e.target.value);
+    countRef.current++; // 수정 횟수를 저장 확인 할 수 있다.
+    console.log(countRef.current);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
-      //   name : hi
-      //  birth : 2024-08-06
-      //  country : kr
-      //  bio : 안녕
     });
   };
 
   return (
     <div>
-      <div>
-        <p>{refObj.current}</p>
-        <button
-          onClick={() => {
-            refObj.current++;
-            console.log(refObj.current);
-            // 값은 변경 되어도 리렌더링이 되지 않아 UI 변경은 없다.
-            // 저장 할때 렌더링 되면 그때 값이 바뀜
-          }}
-        >
-          ref + 1
-        </button>
-      </div>
-
       {/* 1. 이름  */}
       <div>
         <input
