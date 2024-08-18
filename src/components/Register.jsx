6b5c70860f6df93bf1bc75f6494e7b8f0550a7af
kjw <kingcoding2298@gmail.com>
2024-08-18 11:33:59 +0900

    5.9) State로 사용자 입력 관리하기 : state값 객체로 리팩토링
    -  ' ...input ' 스프레드 연산자를 통해 이전값을 유지 시킬 수 있다.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,29 +8,41 @@ import { useState } from "react";
 */
 
 const Register = () => {
-  const [name, setName] = useState("이름"); // 초기값 설정 !!
-  const [birth, setBirth] = useState("");
-  const [country, setCountry] = useState("");
-  const [bio, setBio] = useState("자기소개");
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  console.log(input);
 
   const onChangeName = (e) => {
-    console.log(e.target.value);
-    setName(e.target.value);
+    setInput({
+      ...input, // 스프레드 연산자 => name 이 아닌 birth, country, bio 값을 그대로 유지시킴 / 없을 경우 name 이 입력 될 때마다 값이 사라짐
+      name: e.target.value,
+    });
   };
 
   const onChangeBirth = (e) => {
-    console.log(e.target.value);
-    setBirth(e.target.value);
+    setInput({
+      ...input,
+      birth: e.target.value,
+    });
   };
 
   const onChangeCountry = (e) => {
-    console.log(e.target.value);
-    setCountry(e.target.value);
+    setInput({
+      ...input,
+      country: e.target.value,
+    });
   };
 
   const onChangeBio = (e) => {
-    console.log(e.target.value);
-    setBio(e.target.value);
+    setInput({
+      ...input,
+      bio: e.target.value,
+    });
   };
 
   return (
@@ -38,7 +50,7 @@ const Register = () => {
       {/* 1. 이름  */}
       <div>
         <input
-          value={name}
+          value={input.name}
           onChange={onChangeName}
           placeholder="이름"
           type="text"
@@ -47,12 +59,12 @@ const Register = () => {
 
       {/* 2. 생년월일 */}
       <div>
-        <input value={birth} onChange={onChangeBirth} type="date" />
+        <input value={input.birth} onChange={onChangeBirth} type="date" />
       </div>
 
       {/* 3. 국적 */}
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select value={input.country} onChange={onChangeCountry}>
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
@@ -62,7 +74,7 @@ const Register = () => {
 
       {/* 4. 자기소개 */}
       <div>
-        <textarea value={bio} onChange={onChangeBio}></textarea>
+        <textarea value={input.bio} onChange={onChangeBio}></textarea>
       </div>
     </div>
   );
