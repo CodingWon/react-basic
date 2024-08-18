6f66c74bd01eb99fc7125a74827ef9c6810ce027
kjw <kingcoding2298@gmail.com>
2024-08-18 09:17:09 +0900

    5.5) 이벤트 처리하기
    - SyntheticBaseEvent(합성 이벤트 객체) 의미

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
 const Button = ({ text, color = "black", children }) => {
+  const onClickButton = (e) => {
+    console.log(e);
+    /* 
+     # SyntheticBaseEvent(합성 이벤트 객체)
+        - 모든 브라우저에서의 이벤트 객체를 하나로 통일한 형태
+     */
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
