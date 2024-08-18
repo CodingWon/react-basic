2b78484bd89450556febc54225392a7e0d94e692
kjw <kingcoding2298@gmail.com>
2024-08-18 09:24:11 +0900

    5.6) State로 상태관리하기 - State 실습 준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
 
 /*
   컴포넌트 규칙
     - 첫글자 대문자
 */
 
-const buttonProps = {
-  text: "카페",
-  color: "blue",
-  a: 1,
-  b: 2,
-  c: 3,
-};
-
 function App() {
   // 부모 컴포넌트 (최상의 조상 - Root 컴포넌트)
-  return (
-    <>
-      <Button text={"메일"} color={"red"} a={1} b={2} c={3} />
-      <Button {...buttonProps} />
-      <Button text={"블로그"}>
-        <div>자식 요소</div>
-        <Header />
-      </Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
