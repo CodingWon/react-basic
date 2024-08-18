3d4295c63bffc7a516226c4c1ac2cae8d503d734
kjw <kingcoding2298@gmail.com>
2024-08-18 10:33:44 +0900

    5.7) State와 Props - 리렌더링 되는 상황
    - 이슈사항 : 리렌더링 성능 문제
    (1) 부모 컴포넌트(App) 에서 숫자를 증가 시키는 state 의 값인 count 가 변경될때 부모 컴포넌트가 리렌더링 된다.
    
    (2)  부모 컴포넌트가 리렌더링 되면 자식 컴포넌트도 리렌더링 된다.
    
    ** 문제점 : 성능이 안좋아진다.
    
    @@ 해결책 : 관련 없는 state 는 분리하는 게 좋다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
 import "./App.css";
 import { useState } from "react";
 
+/*
+  리렌더링 되는 상황
+  1. 자신이 관리하는 state 값이 변경 됐을 때 re-Rendering 이 된다.
+  2. 자신이 제공 받는 props 값이 변경 될때 re-Rendering
+  3. 부모 컴포넌트가 re-redering 되면 자식 컴포넌트도 re-redering 된다.
+*/
+
+const Bulb = ({ light }) => {
+  /*
+    상황 : 
+    1. count 값이 바뀌면 App.jsx 가 리렌더링 된다.
+    3. Bulb 는 자식 컴포넌트로 부모가 리렌더링 되어 같이 리렌더링 된다.
+
+    => Bulb 와 같은 부모가 바뀌면 자동으로 렌더링 되는 컴포넌트가 많이지면
+    성능이 않좋아진다. 
+
+    * 해결책 :  관련 없는 state 는 분리하는게 좋다.
+  */
+  console.log(light);
+  return (
+    <div>
+      {light === "OFF" ? (
+        <h2 style={{ backgroundColor: "orange" }}>ON</h2>
+      ) : (
+        <h2 style={{ backgroundColor: "gray" }}>OFF</h2>
+      )}
+    </div>
+  );
+};
+
 function App() {
   // const state = useState(0); // 초기값 : 0
   const [count, setCount] = useState(0);
@@ -17,8 +47,8 @@ function App() {
     ex_light = ex_light === "OFF" ? "ON" : "OFF";
   };
 
-  console.log("일반 변수 : ", ex_light);
-  console.log("state 변수 : ", light);
+  // console.log("일반 변수 : ", ex_light);
+  // console.log("state 변수 : ", light);
 
   return (
     <>
@@ -28,7 +58,7 @@ function App() {
           리액트는 state 변수를 감지하여 리렌더링 되며 값이 변화한다. 즉, App
           컴포넌트를 다시 호출하여 새로운 state 값을 화면에 보여줌
         </p>
-        <h2>{light}</h2>
+        <Bulb light={light} />
         <button
           onClick={() => {
             setLight(light === "OFF" ? "ON" : "OFF");
